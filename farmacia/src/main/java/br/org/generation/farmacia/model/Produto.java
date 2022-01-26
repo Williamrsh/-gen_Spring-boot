31c0f1e43a8af4ec2f23626dbc588c447d9f5b11
Williamrsh <wrodrigues.santos@outlook.com>
2022-01-25 23:32:59 -0300

    Back End da farmacia

---
+++ farmacia/src/main/java/br/org/generation/farmacia/model/Produto.java
@@ -0,0 +1,111 @@
+package br.org.generation.farmacia.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table (name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(max=100)
+	private String nome;
+	
+	@UpdateTimestamp
+	private LocalDateTime data_cadastro;
+	
+	@Column(name = "data_validade")
+	@JsonFormat(pattern = "yyyy-mm-dd")
+	private LocalDate data_validade;
+	
+	@NotNull
+	private int quantidade;
+	
+	@NotNull
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@Positive(message = "O preço deve ser maior do que zero!")
+	private BigDecimal preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDateTime getData_cadastro() {
+		return data_cadastro;
+	}
+
+	public void setData_cadastro(LocalDateTime data_cadastro) {
+		this.data_cadastro = data_cadastro;
+	}
+
+	public LocalDate getData_validade() {
+		return data_validade;
+	}
+
+	public void setData_validade(LocalDate data_validade) {
+		this.data_validade = data_validade;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+}
